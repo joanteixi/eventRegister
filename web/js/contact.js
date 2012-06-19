05ef1241f3dcc7d8733d1919717e14758e705a4b
Joan Teixidó <joan@laiogurtera.com>
2012-06-19 10:51:16 +0200

    Design modifications and added iframe to load the gmap via ajax.

--- web/js/contact.js
+++ web/js/contact.js
@@ -32,5 +32,16 @@ $(document).ready(function () {
 });
 
 $('#myModal').on('show', function () {
-    $('#body-mapa').load('http://maps.google.es');
+
+    iframe = $('<iframe/>', {
+        'name': 'gmap',
+        'src' : "https://maps.google.com/maps?q=Casa+del+Mar+de+Barcelona,+Calle+de+Albareda,+Barcelona,+Espa%C3%B1a&hl=ca&ie=UTF8&ll=41.37366,2.173877&spn=0.050561,0.077162&sll=37.0625,-95.677068&sspn=54.005807,79.013672&oq=casa+del+mar+barcelona&t=w&hq=Casa+del+Mar&hnear=Carrer+d'Albareda,+08004+Barcelona,+Catalunya,+Espanya&z=14&output=embed",
+        'id'  : 'map',
+        'width' : '100%',
+        'height' : '450px'
+    });
+    $('#body-mapa').html(iframe);
 });
+
+
+
